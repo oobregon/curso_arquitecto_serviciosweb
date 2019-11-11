17fe8d261bb6ab92fbca32cc36c9272a5dea3954
tarde <tarde@G24-EQ09>
2019-11-11 20:45:36 +0100

    Actualizando

--- 05_cliente_sw_provincias/src/principal/PruebaProvincias.java
+++ 05_cliente_sw_provincias/src/principal/PruebaProvincias.java
@@ -2,6 +2,7 @@ package principal;
 
 import java.rmi.RemoteException;
 
+import org.apache.axis.message.MessageElement;
 import org.tempuri.OVCServWeb.OVCCallejero.Callejero_x0020_de_x0020_la_x0020_sede_x0020_electrónica_x0020_del_x0020_catastroSoap;
 import org.tempuri.OVCServWeb.OVCCallejero.Callejero_x0020_de_x0020_la_x0020_sede_x0020_electrónica_x0020_del_x0020_catastroSoapProxy;
 
@@ -9,13 +10,16 @@ import es.meh.catastro.www.Provincias;
 
 public class PruebaProvincias {
 
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws Exception {
 		Callejero_x0020_de_x0020_la_x0020_sede_x0020_electrónica_x0020_del_x0020_catastroSoap px;
 		Callejero_x0020_de_x0020_la_x0020_sede_x0020_electrónica_x0020_del_x0020_catastroSoapProxy proxy = new Callejero_x0020_de_x0020_la_x0020_sede_x0020_electrónica_x0020_del_x0020_catastroSoapProxy();
 		px = proxy.getCallejero_x0020_de_x0020_la_x0020_sede_x0020_electrónica_x0020_del_x0020_catastroSoap();
 		
 		Provincias provincias = px.obtenerProvincias();
-
+		MessageElement[] elementos = provincias.get_any();
+		for (MessageElement m:elementos) {
+			System.out.println(m.getAsString());
+		}
 	}
 
 }
