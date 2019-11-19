f6302a6307988bf3161e1f21a243106f05fb2198
oobregon <oobregonmartin@gmail.com>
2019-11-19 14:35:00 +0100

    pequeños cambios

--- 16_ejemplo_spring_boot/src/main/java/inicio/Application.java
+++ 16_ejemplo_spring_boot/src/main/java/inicio/Application.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan (basePackages = {"controller"})
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
